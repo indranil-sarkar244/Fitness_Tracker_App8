13edcd8efc892f8de4b6cc0780b72750921cac8f
JD15052005 <kumarjaydev623@gmail.com>
2025-02-17 00:00:06 +0530

    Initial Changes

--- app/src/main/java/FitnessTracker_databaseexample/fitnessDB.java
+++ app/src/main/java/FitnessTracker_databaseexample/fitnessDB.java
@@ -7,13 +7,17 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class fitnessDB extends SQLiteOpenHelper {
+    private  static final String DATABASE_NAME="FitnessDB";
+    private  static final int DATABASE_VERSION= 1;
+
 
     public fitnessDB(Context context) {
-        super(context, name, factory, version);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
 
     }
 

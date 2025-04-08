12e5bf89b4b193af858d2c667d3a9fe2a670504a
mandipj15 <mandipjadav463@gmail.com>
2025-04-08 19:01:52 +0530

    fit API integrate changes

--- app/src/main/java/com/example/FitnessTracker/MainActivity.java
+++ app/src/main/java/com/example/FitnessTracker/MainActivity.java
@@ -4,21 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.renderscript.Element;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-//fit api lybreris
-import com.google.android.gms.fitness.FitnessOptions;
-import com.google.android.gms.fitness.data.DataType;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         new  Handler(Looper.getMainLooper()).postDelayed(new Runnable() { //Handler as a tool that helps run some code after a delay.  Handler is used to schedule tasks to be executed on a specific thread.
             // Looper.getMainLooper() makes sure this happens on the main UI thread (so the app doesn’t freeze).
-           // postDelayed : This tells the app: "Wait for 3 seconds, then do something."
+            // postDelayed : This tells the app: "Wait for 3 seconds, then do something."
             // 10000 means 3000 milliseconds, which is 10 seconds.
             @Override
             public void run() { //The run() method runs.
@@ -37,16 +30,5 @@ public class MainActivity extends AppCompatActivity {
                 finish();//finish(); closes MainActivity, so when the user presses the back button, they won't return to it.
             }
         }, 3000);
-
     }
-//   GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//        .requestEmail
-//                       new Scope(Scopes.FITNESS_LOCATION_READ_WRITE))
-//        .build();
-   FitnessOptions fitnessOptions =
-        FitnessOptions.builder()
-                .addDataType(Element.DataType.RS_FONT, FitnessOptions.ACCESS_READ)
-                .build();
-
-
 }
